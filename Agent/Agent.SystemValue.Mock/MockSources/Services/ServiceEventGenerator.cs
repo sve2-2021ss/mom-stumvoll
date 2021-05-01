0b99969c408ddd0a9a0274c04160ff8c4ca53e2d
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-05-01 17:13:11 +0200

    fix bug where stopped services would be stopped again

--- Agent/Agent.SystemValue.Mock/MockSources/Services/ServiceEventGenerator.cs
+++ Agent/Agent.SystemValue.Mock/MockSources/Services/ServiceEventGenerator.cs
@@ -1,24 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Agent.SystemValue.Api.SystemValueSource;
 using Agent.SystemValue.Api.Types;
 using Agent.SystemValue.Mock.MockSources.Generator;
 
 namespace Agent.SystemValue.Mock.MockSources.Services
 {
-    public class ServiceEventGenerator : ISystemValueSource<ServiceEvent>, ISystemValueGenerator<ServiceEvent>
+    public class ServiceEventGenerator : ISystemValueGenerator<ServiceEvent>
     {
         private readonly int _sleepLower;
         private readonly int _sleepUpper;
         private readonly Random _random = new();
-        private readonly IList<string> _serviceNames;
+        private readonly IList<string> _stoppedServices;
         private readonly IList<string> _runningServices = new List<string>();
 
         public ServiceEventGenerator(int sleepLower, int sleepUpper, IList<string> serviceNames)
         {
-            _serviceNames = serviceNames;
+            _stoppedServices = serviceNames;
             _sleepUpper = sleepUpper;
             _sleepLower = sleepLower;
         }
@@ -30,29 +30,29 @@ namespace Agent.SystemValue.Mock.MockSources.Services
             switch (d)
             {
                 case < 0.33:
-                    if (_runningServices.Count != 0)
+                    if (_runningServices.Any())
                     {
                         var stoppedService = GetRandomElement(_runningServices);
                         _runningServices.Remove(stoppedService);
-                        NewSystemValue?.Invoke(new ServiceEvent(stoppedService, ServiceEventType.Start));
+                        _stoppedServices.Add(stoppedService);
+                        NewSystemValue?.Invoke(new ServiceEvent(stoppedService, ServiceEventType.Stop));
                     }
 
                     break;
-
                 case < 0.66:
-                    var startedService = GetRandomElement(_serviceNames);
-                    _runningServices.Add(startedService);
-                    NewSystemValue?.Invoke(new ServiceEvent(startedService, ServiceEventType.Stop));
+                    if (_stoppedServices.Any())
+                    {
+                        var startedService = GetRandomElement(_stoppedServices);
+                        _runningServices.Add(startedService);
+                        _stoppedServices.Remove(startedService);
+                        NewSystemValue?.Invoke(new ServiceEvent(startedService, ServiceEventType.Start));
+                    }
+
                     break;
             }
         }
 
-        private string GetRandomElement(IList<string> list)
-        {
-            var index = _random.Next(list.Count);
-            var elem = list[index];
-            return elem;
-        }
+        private string GetRandomElement(IList<string> list) => list[_random.Next(list.Count)];
 
         public Task Run(CancellationToken token) =>
             Task.Run(async () =>
